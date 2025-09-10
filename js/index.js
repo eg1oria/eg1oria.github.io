e75d168f9270d60bb902a407c139535a8e630c87
eg1oria <leontevegor57@gmail.com>
2025-09-10 20:45:05 +0500

    sdfsd

--- js/index.js
+++
@@ -1,8 +0,0 @@
-import { errorInputs } from "../js/inputs.js";
-import { modalSuccess, modalInfo } from "./modal.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-    errorInputs();
-    modalSuccess();
-    modalInfo();
-});
